d8bbdad6fe7a9db166b7c18ea710c21df94cfc9f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-20 23:37:18 +0100

    Add users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,27 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Sign Up Successfully'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Name already exists OR Name is blank'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :fullname, :photo, :cover_image)
   end
 end
