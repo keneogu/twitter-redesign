d8d65fbe12d8f1cbc800d055d870dbce6d090de7
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-20 15:39:13 +0100

    Add user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_20_133813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "fullname", default: "", null: false
+    t.string "photo"
+    t.string "cover_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
