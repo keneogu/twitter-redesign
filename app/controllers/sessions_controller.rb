e3e87a584cb6f3b320230194d4f0064d059f6228
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-21 01:32:21 +0100

    Add sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(name: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to root_path(@user)
+    else
+      flash.now[:notice] = 'Please enter valid name'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged Out'
+    redirect_to root_path
+  end
 end
