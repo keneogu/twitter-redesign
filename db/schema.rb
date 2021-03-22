951d98a59fd934dd67848d0e67f6e34e8c0ac2fe
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-22 11:12:13 +0100

    Migrate tweets database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_133813) do
+ActiveRecord::Schema.define(version: 2021_03_22_094615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.text "text", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
