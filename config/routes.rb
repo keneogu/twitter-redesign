732d35a18af15589097401995c19dc23e89efeb6
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-22 11:20:16 +0100

    Add tweets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
   root 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index,:new,:create,:show]
