02b87fbdb5bc6ce418aa46bb77d859c570afb0c7
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-25 00:30:45 +0100

    Modified routes file for likes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   end
 
   resources :relationships, only: [:destroy]
+
+  resources :tweets, only: [:index, :create] do
+    resources :likes, only: [:create, :destroy]
+  end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
