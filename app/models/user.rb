b4162ccc482957e3305bb3008d2624ae90853036
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-24 01:09:15 +0100

    Add relationship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
 	has_many :tweets, foreign_key: :author_id, dependent: :destroy
-
 	
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followers, through: :passive_relationships
+
 	validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
 end
