ef9756c1b59d2f8e89363c507bc6a7d880f659af
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-24 11:47:47 +0100

    Add users following relationship model

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index,:new,:create,:show]
+
+  resources :users, only: %i[show] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
