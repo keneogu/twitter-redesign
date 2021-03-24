7ea58a06a33f3b89787b9786d3a91b6c76a8c54e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-25 00:44:59 +0100

    Add likes model associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
 	belongs_to :author, class_name: 'User'
+	has_many :likes, dependent: :destroy
 
 	scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 	validates :text, presence: true, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 	has_many :followers, through: :passive_relationships
 
+	has_many :likes, dependent: :destroy
+
 	validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
 
 	def follow(other_user)
