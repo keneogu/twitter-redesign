9a7470730c57574de2a3cffd613a7391fe769211
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-26 11:18:50 +0100

    Add styling for home page and profile page

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -4,11 +4,11 @@ module RelationshipsHelper
 	
 		if current_user.followings.exists?(user.id)
 		  form_with url: relationship_path(user), method: :delete do |form|
-		  form.submit 'Unfollow', class: " px-4 py-1 bg-danger text-light rounded-pill text-red-500 border border-red-500 rounded-full "
+		  form.submit 'Unfollow', class: " px-4 py-1 bg-primary text-light rounded-pill text-red-500 border rounded-full btn-sm"
 		  end
 		else
 		  form_with url: user_relationships_path(user) do |form|
-	      form.submit 'Follow', class: " px-4 py-1 bg-primary text-light rounded-pill text-red-500 border border-red-500 rounded-full "
+	      form.submit 'Follow', class: " px-4 py-1 bg-primary text-light rounded-pill text-red-500 border btn-sm rounded-full "
           end
 		end
 	end
